2462b2dff485ed0c09dae7a505bd2bdb8f4d4aeb
Prejan Neupane <neupaneprejan2018@gmail.com>
2023-06-19 16:22:00 +0545

    after adding dynamic memory allocation

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 //function for swapping 
 void swap_element(int* A, int* B){
-    int temp;
-    temp =* A;
-    *A = *B;
-    *B=temp;
+    *A = *A+*B;
+    *B=*A-*B;
+    *A=*A-*B;
 }
 //function for sorting the array
 void bubble_sort(int * ptr,int size){
+    clock_t start, end;
+    start = clock();
     for (int i=0;i<=size-1;i++){
         for (int j=0;j<size-i-1;j++){
             if (*(ptr+j)>*(ptr+j+1)){
@@ -16,23 +19,27 @@ void bubble_sort(int * ptr,int size){
             }
         }
     }
+    end = clock();
+    double duration = ((double)end - start)/CLOCKS_PER_SEC;
+    printf("Time taken to execute in seconds : %f", duration);
 }
 int main(){
-    int arr[100];
+    
     int size;
     int *ptr;
-    ptr=&arr[0];
     printf("Enter the size of the array \n");
     scanf("%d",&size);
+    ptr = (int*)malloc(size * sizeof(int));
     printf("Enter the array \n");
     for (int i=0;i<size;i++){
         printf("Array[%d] : ",i);
-        scanf("%d",&arr[i]);
+        scanf("%d",&ptr[i]);
     }
     bubble_sort(ptr,size);
     printf("After sorting the array \n");
     for (int i=0;i<size;i++){
-        printf("%d  ",arr[i]);
+        printf("%d  ",ptr[i]);
     }
+    
     return 0;
 }
